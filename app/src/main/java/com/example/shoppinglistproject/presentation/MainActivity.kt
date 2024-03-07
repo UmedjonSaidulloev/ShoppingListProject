41ae627dd8b085564b2106b539d08e19d3fbad8a
Umedjon Saidulloev <umedjon.saidulloev@mail.ru>
2024-03-07 10:31:40 +0500

    Lesson4

--- app/src/main/java/com/example/shoppinglistproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistproject/presentation/MainActivity.kt
@@ -6,10 +6,11 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglistproject.R
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
