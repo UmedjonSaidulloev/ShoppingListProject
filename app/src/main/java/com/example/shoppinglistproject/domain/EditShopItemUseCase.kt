cc3183b90dca52f2f2680db8c53373aed3324a56
Umedjon Saidulloev <umedjon.saidulloev@mail.ru>
2024-03-07 08:51:33 +0500

    Lesson1

--- app/src/main/java/com/example/shoppinglistproject/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglistproject/domain/EditShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglistproject.domain
 
-class EditShopItemUseCase {
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
     fun editShopItem(shopItem: ShopItem){
-
+        shopListRepository.editShopItem(shopItem)
     }
 }
